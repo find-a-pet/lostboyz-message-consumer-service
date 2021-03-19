9383e779e54cc378025213cb06151abe017c7758
Michael Mkwelele <memkwelele@gmail.com>
2021-03-19 12:34:35 -0700

    Initialize repository.

---
+++ src/main/java/com/mtotowamkwe/lostboyzmessageconsumerservice/LostboyzMessageConsumerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mtotowamkwe.lostboyzmessageconsumerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostboyzMessageConsumerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostboyzMessageConsumerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtotowamkwe/lostboyzmessageconsumerservice/LostboyzMessageConsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtotowamkwe.lostboyzmessageconsumerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostboyzMessageConsumerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
