19dc49e22604bd5adee3de6b6361fa834e20d663
Michael Mkwelele <15795163+mtotoWamkwe@users.noreply.github.com>
2021-03-26 14:46:12 -0700

    Update email service endpoint.

--- src/main/java/com/mtotowamkwe/lostboyzmessageconsumerservice/util/Constants.java
+++ src/main/java/com/mtotowamkwe/lostboyzmessageconsumerservice/util/Constants.java
@@ -2,12 +2,7 @@ package com.mtotowamkwe.lostboyzmessageconsumerservice.util;
 
 public class Constants {
 
-    public static final String API_MESSAGE_CONSUMER_ENDPOINT = "/api/v1/consume";
-
-    // TODO: Update URLs to match production endpoints prior to deployment
-    public static final String LOSTBOYZ_EMAIL_SERVICE_URL = "http://127.0.0.1:14000/api/v1/email";
+    public static final String LOSTBOYZ_EMAIL_SERVICE_URL = "https://lostboyz-email-service.herokuapp.com/api/v1/email";
 
     public static final String TEST_BAD_LOSTBOYZ_EMAIL_SERVICE_URL = "htp:/127001:14000/api/v1/email^^~@";
-
-    public static final String TEST_MESSAGE_TO_BE_ENQUEUED = "alice@lostboyz.com,123456";
 }
